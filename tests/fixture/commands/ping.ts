08857235726369d212373df3447f8b1a4ac007b8
ShadAeons <shad.aeons@gmail.com>
2026-05-06 21:53:09 +0200

    test: deleted fixtures

--- tests/fixture/commands/ping.ts
+++
@@ -1,7 +0,0 @@
-import { defineCommand } from '../../../src/types/Command';
-
-export default defineCommand('ping', {
-    async execute(interaction) {
-        await interaction.reply('Pong!');
-    },
-});
--- tests/fixture/events/ready.ts
+++
@@ -1,7 +0,0 @@
-import { defineEvent } from '../../../src/types/Event';
-
-export default defineEvent('clientReady', {
-    execute(client) {
-        console.log(client.user);
-    },
-});
