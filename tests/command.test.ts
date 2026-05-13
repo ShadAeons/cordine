9180e650f8a4e3e33ab1540237d81d35cc1cdcc9
Shad <shad.aeons@gmail.com>
2026-05-13 23:01:24 +0200

    tests: changed tests to check for FlatCommandConfig and SubcommandCommandConfig validity

--- tests/command.test.ts
+++ tests/command.test.ts
@@ -2,9 +2,9 @@ import { describe, expect, it, vi } from 'vitest';
 import { defineCommand } from '../src/types/command';
 
 describe('Command', () => {
-    it('should create a CommandConfig', () => {
+    it('should create a FlatCommandConfig', () => {
         const execute = vi.fn();
-        const event = defineCommand('ping', {
+        const cmd = defineCommand('ping', {
             description: 'Ping command',
             options: {
                 target: { type: 'user', description: 'Target user' },
@@ -12,21 +12,32 @@ describe('Command', () => {
             execute,
         });
 
-        expect(event.name).toBe('ping');
-        expect(event.description).toBe('Ping command');
-        expect(event.options.target).toMatchObject({
+        expect(cmd.type).toBe('flat');
+        expect(cmd.name).toBe('ping');
+        expect(cmd.description).toBe('Ping command');
+        expect(cmd.options.target).toMatchObject({
             type: 'user',
             description: 'Target user',
         });
-        expect(event.execute).toBe(execute);
+        expect(cmd.execute).toBe(execute);
     });
 
-    it('should fall back to default values', () => {
-        const event = defineCommand('ping', {
-            execute: vi.fn(),
+    it('should create a SubcommandCommandConfig', () => {
+        const execute = vi.fn();
+        const cmd = defineCommand('ping', {
+            description: 'Ping command',
+            subcommands: {
+                sub: { description: '', options: {}, execute },
+            },
         });
 
-        expect(event.description).toBe('Default command description');
-        expect(event.options).toMatchObject({});
+        expect(cmd.type).toBe('subs');
+        expect(cmd.name).toBe('ping');
+        expect(cmd.description).toBe('Ping command');
+        expect(cmd.subcommands.sub).toMatchObject({
+            description: '',
+            options: {},
+            execute,
+        });
     });
 });
